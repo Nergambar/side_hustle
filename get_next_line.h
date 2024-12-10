63caec9a4f15b03e885608697a499c7fe1b35668
Neri Gambarelli <negambar@c1r3p4.42firenze.it>
2024-12-10 16:26:23 +0100

    .h

--- get_next_line.h
+++ get_next_line.h
@@ -3,51 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alerusso <alerusso@student.42.fr>          +#+  +:+       +#+        */
+/*   By: negambar <negambar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/09 16:13:36 by alerusso          #+#    #+#             */
-/*   Updated: 2024/12/09 17:15:17 by alerusso         ###   ########.fr       */
+/*   Created: 2023/11/24 14:43:14 by negambar          #+#    #+#             */
+/*   Updated: 2023/12/14 11:50:50 by negambar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Memoria statica usata ---> BUFFER_SIZE * 4000
-// Define per il debug
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1000
+#  define BUFFER_SIZE 2
 # endif
-# define COUNTER 1000
-# define FORCE_READ_ONE_TEXT 0
-// Define usati nel programma
-# define MALLOC 1
-# define REALLOC 2
-# define FREE 3
-# define CALLOC 4
-# define MEMCPY 5
-# define FULL_MEMORY -1
-# define SUCCESS 1
-# define NO_NEWLINE_FOUND 0
-# define EOF_OR_NEWLINE_FOUND 1
-# define EOF_OR_NEWLINE_NOT_FOUND -1
-# define EMPTY_BUFFER 0
-# define END_OR_CORRUPTION -1
-// Librerie
 # include <unistd.h>
-# include <stdio.h>
-# include <malloc.h>
+# include <stdlib.h>
 # include <fcntl.h>
-# include <limits.h>
+# include <stdio.h>
 
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(char *s);
+char	*ft_substr(char const *s, unsigned int start, char stop_char);
+char	*ft_calloc(size_t count, size_t size);
+char	*ft_get_line(int fd, char *buffer, char *nextl);
 char	*get_next_line(int fd);
-char	*get_next_line_main_function(int fd, char buffer[BUFFER_SIZE + 1]);
-char	*get(char **store_bytes, char buffer[BUFFER_SIZE + 1], int nl, int fd);
-int		go_read(int fd, char buffer[BUFFER_SIZE + 1], char **new_line);
-int		loop_read(char **new_line, int *control_read, size_t *end, int fd);
-int		alloc_ft(void **content, void *new_content, size_t start, int mode);
-int		find_end_line(size_t *start, char *string);
-void	*calloc_memcpy(int size, void *dest, const void *src, int ft);
-void	trim_readbytes(char *buffer);
-char	*ft_strjoin(char *s1, char *s2);
+char	*ft_str(char *s);
 
 #endif
