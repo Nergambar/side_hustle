765107406d8eee903dc1ee163bdeae739c3b4f5e
Neri Gambarelli <negambar@c2r4p5.42firenze.it>
2024-12-14 16:56:49 +0100

    leaksgoski

--- main.c
+++ main.c
@@ -23,39 +23,6 @@ static void freeall(all *this)
 	free(this);
 }
 
-/* static void set_basics(all *this, int fd)
-{
-	printf("Narrator: Welcome, uh.. what's your name again? ");
-	this->p->name->first_name = calloc(100, sizeof(char));
-	fgets(this->p->name->first_name, 100, stdin);
-	size_t len = strlen(this->p->name->first_name);
-	if (len > 0 && this->p->name->first_name[len - 1] == '\n')
-		this->p->name->first_name[len - 1] = '\0';
-	size_t size = strlen(this->p->name->first_name) + 1;
-	this->p->name->first_name = realloc(this->p->name->first_name, size);
-	char full[101];
-	snprintf(full, sizeof(full), "Name: %s\n", this->p->name->first_name);
-	printf("%s", this->p->name->first_name);
-	if (!write_in_file(full, fd, 0))
-		printf("something went wrong!\n");
-	printf("\nright, %s, finally you're here\n", this->p->name->first_name);
-	printf("I'm sorry to ask, dear friend, but how old are you exactly? ");
-	scanf("%zu", &this->p->age);
-	snprintf(full, sizeof(full), "Age: %zu\n", this->p->age);
-	if (this->p->age <= 17)
-	{
-		printf("I'm sorry little one, you aren't who I thought you were.\
-Come back when you're older.\n\n\n\n\n");
-		reset(this, NULL);
-		return ;
-	}
-	if (!write_in_file(full, fd, 0))
-		printf("something went wrong!\n");
-	close(fd);
-} */
-
-
-
 static void set_basics(all *this, int fd)
 {
 	char full[101];
@@ -95,11 +62,11 @@ Come back when you're older.\n\n\n\n\n");
         reset(this, NULL);
         return;
     }
-	write(1, "okaay lesgo", 12);
-	sleep(5);
+	write(1, "very well, then, shall we start?", 33);
+	sleep(3);
 	system("clear");
     close(fd);
-	second_step();
+	// second_step();
 }
 
 void reset(all *this, char *s)
@@ -121,7 +88,6 @@ int main(int ac, char **av)
 	all		*this;
 	int		fd = open("player_info.txt", O_RDWR);
 	char	*s = gnl(fd);
-	// char	full[101];
 
 	this = (all *)calloc(sizeof(all), 1);
 	initall(this);
@@ -130,15 +96,23 @@ int main(int ac, char **av)
 		set_basics(this, fd);
 	else if (s && ac == 1)
 	{
+		char *mynameis;
 		name->first_name = get_that_line(fd, "Name = ", s);
 		// printf("\ns:%s", s);
 		if (!name->first_name)
 			return (free(s), freeall(this), 1);
-		name->first_name = strtrim(name->first_name, "\n");
+		mynameis = name->first_name;
+		name->first_name = strtrim(mynameis, "\n");
+		free(mynameis);
+		if (!name->first_name)
+			return (free(s), freeall(this), 1);
 		printf("Ah, yes,%s, welcome back!\n", name->first_name);
 	}
 	else if (strcmp(av[1], "reset") == 0)
 		return(reset(this, s), 1);
+	else if (strcmp(av[1], "debug") == 0)
+		fd = open("player_info.txt", O_TRUNC);
+	free(s);
 	close(fd);
 	freeall(this);
 	return (0);
